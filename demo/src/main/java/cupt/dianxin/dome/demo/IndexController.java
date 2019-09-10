de541f4a1c31b06cfa52c541f463b3d4a813ab0a
wxw93 <chen_wei_wei@163.com>
2019-09-10 17:18:13 +0800

    提交测试

---
+++ demo/src/main/java/cupt/dianxin/dome/demo/IndexController.java
@@ -0,0 +1,16 @@
+package cupt.dianxin.dome.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/login")
+public class IndexController {
+
+    @RequestMapping(value = "index")
+    @ResponseBody
+    public String index(){
+        return "hello idea!";
+    }
+}
